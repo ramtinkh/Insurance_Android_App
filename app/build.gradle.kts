3b8e255a6648ffdd9d8a24589fc3d928fab0e702
Mana <man.peiravian@gmail.com>
2024-07-03 02:11:22 +0330

    minor fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,14 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation("com.google.firebase:firebase-auth:21.1.0")
+    implementation("com.google.firebase:firebase-inappmessaging-display:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 }
--- app/src/main/java/com/example/insurance_android_app/Splash.java
+++ app/src/main/java/com/example/insurance_android_app/Splash.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Handler;
 
 
-public class splash extends AppCompatActivity {
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +17,7 @@ public class splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splash.this, MainActivity.class);
+                Intent intent = new Intent(Splash.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
