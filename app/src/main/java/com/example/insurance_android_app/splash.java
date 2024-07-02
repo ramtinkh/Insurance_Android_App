dd4671fead46dbf04f6e7bbf71529277dcecb296
Mana <man.peiravian@gmail.com>
2024-07-02 20:43:07 +0330

    add splash screen

---
+++ app/src/main/java/com/example/insurance_android_app/splash.java
@@ -0,0 +1,26 @@
+package com.example.insurance_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
