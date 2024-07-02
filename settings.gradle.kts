4759256c6aa1eb140fefacafda5bb33ade15b77b
ramtinkh <ramtinkhoshnevis1@gmail.com>
2024-07-02 11:22:20 +0330

    add: initial project added.

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Insurance_Android_App"
+include(":app")
